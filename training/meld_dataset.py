c68d4b04dd19648861efb15090cdeb5f989053cc
Abhsiehk T G <1si22cs006@sit.ac.in>
2025-09-07 13:34:59 +0530

    Video frames extraction Done

---
+++ training/meld_dataset.py
@@ -0,0 +1,111 @@
+from torch.utils.data import Dataset
+import pandas as pd
+from transformers import AutoTokenizer
+import os
+import cv2
+import numpy as np
+import torch
+
+# Disable parallelism warnings & SSL verification
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+# os.environ['HF_HUB_DISABLE_SSL_VERIFY'] = '1'
+
+
+class MELDDataset(Dataset):
+    def __init__(self, csv_path, video_dir):
+        self.data = pd.read_csv(csv_path)
+
+        self.video_dir = video_dir
+
+        #  Correct indentation here
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            r"C:\Users\v-abhishek.tg\OneDrive - Aurigo Software Technologies Inc\Desktop\MP\models\bert-base-uncased"
+        )
+
+        self.emotion_map = {
+            'anger': 0,
+            'disgust': 1,
+            'fear': 2,
+            'joy': 3,
+            'neutral': 4,
+            'sadness': 5,
+            'surprise': 6
+        }
+
+        self.sentiment_map = {
+            'negative': 0,
+            'neutral': 1,
+            'positive': 2
+        }
+
+    def _load_video_frames(self , video_path):
+        cap = cv2.VideoCapture(video_path)
+        frames = []
+
+        try:
+            if not cap.isOpened():
+                raise ValueError(f"Error opening video file: {video_path} ")
+            # Try and read the first frame to validate video 
+            ret, frame = cap.read()
+            if not ret or frame is None:
+                raise ValueError(f"Error reading frame from video file: {video_path}")
+            # reset index to start
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            
+            while len(frames) < 30 and cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                frame = cv2.resize(frame , (224,224))
+                frame = frame / 255.0  # Normalize pixel values to [0, 1]
+                frames.append(frame)
+
+
+        except Exception as e:
+            raise ValueError(f"Video error:{str(e)}")
+        finally:
+            cap.release()
+
+        if(len(frames) == 0):
+                raise ValueError(f"No frames extracted from video: {video_path}")
+        # Ensure at least 30 frames are present else add dummy frames
+        if(len(frames) < 30):
+            frames+= [np.zeros_like(frames[0])]*(30-len(frames)) 
+        else:
+            frames = frames[:30]
+        # before permute : [frames , height , width , channels] 
+        # after permute : [frames , channels , height , width]
+        return torch.FloatTensor(np.array(frames)).permute(0,3,1,2)  # Convert to tensor and change to (T, C, H, W)
+            
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]
+        video_filename = f"""dia{row['Dialogue_ID']}_utt{
+            row['Utterance_ID']}.mp4"""
+        path = os.path.join(self.video_dir, video_filename)
+        video_path_exists= os.path.exists(path);
+
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Video file not found: {path}")
+        
+        text_inputs = self.tokenizer(row['Utterance'],
+                                     padding='max_length',
+                                     truncation=True,
+                                     max_length=128,
+                                     return_tensors='pt')
+        
+        video_frames = self._load_video_frames(path)
+                                     
+        print(video_frames)
+
+
+if __name__ == "__main__":
+    meld = MELDDataset(
+        '../dataset/dev/dev_sent_emo.csv',
+        '../dataset/dev/dev_splits_complete'
+    )
+    print(meld[4])
+
